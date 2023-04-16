91f26ee0f0aa5fc19aba3ef0bd9e97b875cc0db8
MZannn <mfauzan1098@gmail.com>
2023-04-16 15:59:33 +0700

    twilio whatsapp

--- app/Http/Controllers/Admin/ReportChangeDeviceController.php
+++ app/Http/Controllers/Admin/ReportChangeDeviceController.php
@@ -7,6 +7,7 @@ use App\Models\Employee;
 use App\Models\ReportChangeDevice;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Twilio\Rest\Client;
 
 class ReportChangeDeviceController extends Controller
 {
@@ -22,10 +23,23 @@ class ReportChangeDeviceController extends Controller
 
     public function approved(Request $request)
     {
+        $sid = config('twilio.account_sid');
+        $token = config('twilio.auth_token');
+        $from = config('twilio.from');
+        $client = new Client($sid, $token);
         if ($request->status == 'APPROVED') {
-            Employee::where('nip', $request->employee_id)->update([
+            $employee = Employee::where('nip', $request->employee_id);
+            $employee->update([
                 'device_id' => null
             ]);
+            $client->messages->create(
+                $request->input($employee->phone_number),
+                array(
+                    'from' => $from,
+                    'body' => 'Permintaan Penggantian Device Anda Telah Disetujui'
+                )
+            );
+
             ReportChangeDevice::findOrFail($request->id)->delete();
             return redirect()->route('reportChangeDevice')->with('alert', 'Berhasil Menyetujui Permintaan');
         } else if ($request->status == 'REJECTED') {
