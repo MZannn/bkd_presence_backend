40fc0ccf1b065b42f6d6c72f2b292c6cdd7741bb
MZannn <mfauzan1098@gmail.com>
2023-04-17 16:13:21 +0700

    parsing current date

--- app/Exports/PresenceExport.php
+++ app/Exports/PresenceExport.php
@@ -33,6 +33,7 @@ class PresenceExport implements FromView
         $current_date = Carbon::parse($start_date);
         while ($current_date->lte(Carbon::parse($end_date))) {
             $calculator = $current_date;
+            
             if (!$calculator->is_holiday() && $current_date->isWeekday()) {
                 $working_days++;
             }
