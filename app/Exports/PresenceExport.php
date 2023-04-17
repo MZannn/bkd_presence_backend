dd990a8e0477271910286b027d4d0c42a3882df3
MZannn <mfauzan1098@gmail.com>
2023-04-18 02:57:32 +0700

    test

--- app/Exports/PresenceExport.php
+++ app/Exports/PresenceExport.php
@@ -64,7 +64,7 @@ class PresenceExport implements FromCollection, WithHeadings, WithMapping
 
         return collect([
             [
-                'nip' => sprintf('%019s', $presence->employee->nip),
+                'nip' => sprintf('%020s', $presence->employee->nip),
                 'nama' => $presence->employee->name,
                 'kantor' => $presence->office->name,
                 'hari_kerja' => $working_days,
