9569d6de74bd05f4abe13ef33896b847d0f3b56f
MZannn <mfauzan1098@gmail.com>
2023-04-18 03:22:28 +0700

    format

--- app/Exports/PresenceExport.php
+++ app/Exports/PresenceExport.php
@@ -62,7 +62,7 @@ class PresenceExport implements FromCollection, WithHeadings, WithMapping
             }
             return collect([
                 [
-                    'nip' =>$presence->employee->nip,
+                    'nip' => $presence->employee->nip,
                     'nama' => $presence->employee->name,
                     'kantor' => $presence->office->name,
                     'hari_kerja' => $working_days,
@@ -95,7 +95,7 @@ class PresenceExport implements FromCollection, WithHeadings, WithMapping
     public function map($row): array
     {
         return [
-            "'".strval($row['nip']),
+            "'" . strval($row['nip']),
             $row['nama'],
             $row['kantor'],
             $row['hari_kerja'],
