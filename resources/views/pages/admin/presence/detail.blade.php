333b10cd08b6fbfca46cbc775854c6f7483d097f
MZannn <mfauzan1098@gmail.com>
2023-05-01 22:19:44 +0700

    km to meter in detail presence

--- resources/views/pages/admin/presence/detail.blade.php
+++ resources/views/pages/admin/presence/detail.blade.php
@@ -70,7 +70,7 @@
                         <tr>
                             <th width="20%">Jarak Presensi Masuk</th>
                             <td>
-                                {{ $item->entry_distance }} KM
+                                {{ $item->entry_distance }}m
                             </td>
                         </tr>
                         <tr>
@@ -82,7 +82,7 @@
                         <tr>
                             <th width="20%">Jarak Presensi Keluar</th>
                             <td>
-                                {{ $item->exit_distance }} KM
+                                {{ $item->exit_distance }}m
                             </td>
                         </tr>
                     @endforeach
