fa31b9ade9f5f9868df1a0a7f9441a93cac166db
MZannn <mfauzan1098@gmail.com>
2023-05-01 11:29:02 +0700

    vacation controller

--- app/Http/Controllers/Admin/VacationController.php
+++ app/Http/Controllers/Admin/VacationController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Office;
 use App\Models\Presence;
 use App\Models\Vacation;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -20,15 +21,91 @@ class VacationController extends Controller
         if (Auth::user() && Auth::user()->roles == 'SUPER ADMIN') {
             $items = Vacation::with(['employee', 'office', 'presence'])->paginate(10);
             if ($request->has('search')) {
-                $items = Presence::with(['office', 'employee'])->where('employee_id', 'like', '%' . $request->search . '%')->paginate(10);
+                $items = Vacation::with(['office', 'employee'])->where('employee_id', 'like', '%' . $request->search . '%')->paginate(10);
             }
         } else {
             $items = Vacation::with(['employee', 'office', 'presence'])->where('office_id', Auth::user()->office_id)->paginate(10);
             if ($request->has('search')) {
-                $items = Presence::with(['office', 'employee'])->where('employee_id', 'like', '%' . $request->search . '%')->where('office_id', Auth::user()->office_id)->paginate(10);
+                $items = Vacation::with(['office', 'employee'])->where('employee_id', 'like', '%' . $request->search . '%')->where('office_id', Auth::user()->office_id)->paginate(10);
             }
         }
-        return view('pages.admin.bussiness-trip.index', compact('items', 'offices'));
+        return view('pages.admin.vacation.index', compact('items', 'offices'));
+    }
+    public function validation(Request $request)
+    {
+
+        $data = Vacation::where('office_id', $request->office_id)
+            ->where('employee_id', $request->employee_id)
+            ->firstOrFail();
+
+        if ($request->status == 'KONFIRMASI') {
+            if ($request->start_date == $request->end_date) {
+                $presence = Presence::where('id', $request->presence_id)
+                    ->where('presence_date', $request->start_date)
+                    ->where('employee_id', $request->employee_id)
+                    ->first();
+                $exists = Presence::where('presence_date', $request->start_date)
+                    ->where('attendance_entry_status', "HADIR")
+                    ->where('employee_id', $request->employee_id)
+                    ->exists();
+                // untuk request 1 hari dan hari kerja
+                if (!$presence && Carbon::parse($request->start_date)->isWeekday() && !$exists) {
+                    Presence::create([
+                        'employee_id' => $request->employee_id,
+                        'office_id' => $request->office_id,
+                        'presence_date' => $request->start_date,
+                        'attendance_entry_status' => "CUTI",
+                        'attendance_exit_status' => "CUTI",
+                    ]);
+                } else if ($presence && Carbon::parse($request->start_date)->isWeekday() && !$exists) {
+                    Presence::findOrFail($request->presence_id)->update([
+                        'presence_date' => $request->start_date,
+                        'attendance_entry_status' => "CUTI",
+                        'attendance_exit_status' => "CUTI",
+                    ]);
+
+                } else if (Carbon::parse($request->start_date)->isWeekend()) {
+                    Vacation::findOrFail($data->id)->delete();
+                    return redirect()->route('vacation')->with('alert', 'Data tidak bisa di validasi karena hari libur');
+                } else if ($exists) {
+                    Vacation::findOrFail($data->id)->delete();
+                    return redirect()->route('vacation')->with('alert', 'Data tidak bisa di validasi karena sudah ada data presensi');
+                }
+                Vacation::findOrFail($data->id)->delete();
+                return redirect()->route('vacation')->with('alert', 'Data berhasil di validasi');
+
+            } else {
+                // untuk request lebih dari 1 hari
+                $start_date = Carbon::parse($request->start_date);
+                $end_date = Carbon::parse($request->end_date);
+                $exists = Presence::where('presence_date', '>=', $request->start_date)->where('presence_date', '<=', $request->end_date)->where('attendance_entry_status', "HADIR")->exists();
+                for ($date = $start_date; $date <= $end_date; $date->addDay()) {
+                    $presence = Presence::where('employee_id', $request->employee_id)->where('presence_date', $date->format('Y-m-d'))->first();
+                    if (!$presence && Carbon::parse($date)->isWeekday() && !$exists) {
+                        Presence::create([
+                            'employee_id' => $request->employee_id,
+                            'office_id' => $request->office_id,
+                            'presence_date' => $date->format('Y-m-d'),
+                            'attendance_entry_status' => "CUTI",
+                            'attendance_exit_status' => "CUTI",
+                        ]);
+                    } else if ($presence && Carbon::parse($date)->isWeekday() && !$exists) {
+                        $presence->update([
+                            'attendance_entry_status' => "CUTI",
+                            'attendance_exit_status' => "CUTI",
+                        ]);
+                    }
+                }
+                Vacation::findOrFail($data->id)->delete();
+                return redirect()->route('vacation')->with('alert', 'Data berhasil di validasi');
+            }
+        } else if ($request->status == 'TOLAK') {
+            Vacation::findOrFail($data->id)->delete();
+            return redirect()->route('vacation')->with('alert', 'Permintaan Cuti ditolak');
+        } else {
+            return redirect()->route('vacation')->with('alert', 'Data pending tidak bisa di validasi');
+        }
+
     }
 
     /**
