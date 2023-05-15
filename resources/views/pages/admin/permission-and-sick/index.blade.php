01c853bce23d579cef8a426fda246db61c9565a3
MZannn <mfauzan1098@gmail.com>
2023-05-15 22:47:32 +0700

    width

--- resources/views/pages/admin/permission-and-sick/index.blade.php
+++ resources/views/pages/admin/permission-and-sick/index.blade.php
@@ -90,7 +90,7 @@
                                                         style="width: 150px" class="img-thumbnail"></a>
                                             @endif
                                         </td>
-                                        <td>
+                                        <td width="20%">
                                             <select class="form-select form-control" name="status" id=""
                                                 aria-label="Default select example">
                                                 <option value="{{ $item->status }}" selected>
