9992830d56fd9ec025e67b01403523a834dcea1d
MZannn <mfauzan1098@gmail.com>
2023-06-20 23:19:40 +0700

    change route

--- resources/views/pages/admin/leave-rules/index.blade.php
+++ resources/views/pages/admin/leave-rules/index.blade.php
@@ -5,7 +5,7 @@
         <!-- Page Heading -->
         <div class="d-sm-flex align-items-center justify-content-between mb-4">
             <h1 class="h3 mb-0 text-gray-800">Jenis Cuti </h1>
-            <a href="{{ route('holiday.create') }}" class="btn btn-sm btn-primary shadow-sm"><i
+            <a href="{{ route('leave-rules.create') }}" class="btn btn-sm btn-primary shadow-sm"><i
                     class="fas fa-plus fa-sm text-white">
                     Tambah Jenis Cuti</i></a>
         </div>
